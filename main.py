20e797dbe22d4156c85a9ed526777a7635103396
err0rgod <ndgaming458@gmail.com>
2025-07-11 00:09:26 +0530

    dedbug

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import argparse
 import socket
 
-ip=input("Enter The IP adresss")
+tar=input("Enter the target")
 p=int(input("Enter the port number"))
 
 
 for port in p:
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.settimeout()
-    result = s.connect_ex((ip,port))
+    result = s.connect_ex((tar,port))
 
     if result == 0:
         print(f"the {port} is Open")
