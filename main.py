18e5a800e0c1699649a45bd52114f70f51cf28d3
err0rgod <ndgaming458@gmail.com>
2025-07-11 00:08:54 +0530

    baasic structure done

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ import socket
 
 ip=input("Enter The IP adresss")
 p=int(input("Enter the port number"))
+
+
+for port in p:
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.settimeout()
+    result = s.connect_ex((ip,port))
+
+    if result == 0:
+        print(f"the {port} is Open")
+    s.close
+
+
+
