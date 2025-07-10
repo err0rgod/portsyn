d7bdefcf1c5159ff7ef3e199daa0ee5d26bb0df8
err0rgod <ndgaming458@gmail.com>
2025-07-11 00:14:06 +0530

    added some more features

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import argparse
 import socket
 
-tar=input("Enter the target")
-p=int(input("Enter the port number"))
+tar=input("Enter the target: ")
+p=int(input("Enter the port number: "))
+ports = range(1,p)
 
 
-for port in p:
+for port in ports:
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.settimeout()
+    s.settimeout(1)
     result = s.connect_ex((tar,port))
 
     if result == 0:
-        print(f"the {port} is Open")
+        print(f"the port {port} is Open")
+    else:
+        print(f"port {port} not open. Failure.")
     s.close
+    
 
 
 
