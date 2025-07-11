2dc594239b095948c461ce3d383f4096f3b1100a
err0rgod <ndgaming458@gmail.com>
2025-07-11 21:50:30 +0530

    error solved

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ import threading
 
 tar=input("Enter the target: ")
 p=int(input("Enter the port number: "))
-t=int(input("Enter number of threads"))
+t=int(input("Enter number of threads: "))
 ports = range(1,p)
 
 
 
 
-def port_scan(tar,ports):
+def port_scan(tar,port):
     try:
-        for port in ports:
-            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            s.settimeout(1)
-            result = s.connect_ex((tar,port))
+        
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.settimeout(1)
+        result = s.connect_ex((tar,port))
 
-            if result :
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)== 0
-                print(f"the port {port} is Open")
-            else:
-                print(f"port {port} not open. Failure.")
-            s.close
+        if result == 0 :
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print(f"the port {port} is Open")
+        else:
+            print(f"port {port} not open. Failure.")
+        s.close()
     
     except Exception as e:
         print(f"Error while scanning port {port} {e}")
@@ -32,13 +32,17 @@ def port_scan(tar,ports):
 def multi_threading(tar,ports,max_threads=10):
     threads = []
     for port in ports:
-        while threading.active_count > max_threads:
+        while threading.active_count() > max_threads:
             pass
 
-        thread  = threading.Thread(target=port_scan, args=(tar,ports))
-        threads.append(thread)
+        thread  = threading.Thread(target=port_scan, args=(tar,port))
         thread.start()
+        threads.append(thread)
+        
 
 
     for thread in threads:
         thread.join()
+
+
+multi_threading(tar,ports,t)
