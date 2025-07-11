51ab1e46f66f33e317a631833bb01333445b08d7
err0rgod <ndgaming458@gmail.com>
2025-07-11 21:24:44 +0530

    error handling

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ p=int(input("Enter the port number: "))
 ports = range(1,p)
 
 
-for port in ports:
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.settimeout(1)
-    result = s.connect_ex((tar,port))
 
-    if result s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)== 0:
-        print(f"the port {port} is Open")
-    else:
-        print(f"port {port} not open. Failure.")
-    s.close
     
 
 
+def port_scan(tar,ports):
+    try:
+        for port in ports:
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.settimeout(1)
+        result = s.connect_ex((tar,port))
 
+        if result s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)== 0:
+            print(f"the port {port} is Open")
+        else:
+            print(f"port {port} not open. Failure.")
+        s.close
+    
+    except Exception as e:
+        print(f"Error while scanning port {port} {e}")
