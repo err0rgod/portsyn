939cffe7e6a805063a9cfb0fa8e17e27eb7f3aff
err0rgod <ndgaming458@gmail.com>
2025-07-11 21:27:47 +0530

    error handled

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ ports = range(1,p)
 def port_scan(tar,ports):
     try:
         for port in ports:
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.settimeout(1)
-        result = s.connect_ex((tar,port))
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.settimeout(1)
+            result = s.connect_ex((tar,port))
 
-        if result s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)== 0:
-            print(f"the port {port} is Open")
-        else:
-            print(f"port {port} not open. Failure.")
-        s.close
+            if result :
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)== 0
+                print(f"the port {port} is Open")
+            else:
+                print(f"port {port} not open. Failure.")
+            s.close
     
     except Exception as e:
         print(f"Error while scanning port {port} {e}")
